5d09380026e9a03236f6279bd3ff9e8eecc57508
Eugene-Firm <eugenekipkoech862@gmail.com>
2026-03-15 13:49:14 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,22 @@
+const settings = {
+  prefixes: ['.', '!', '/', '#'], // Multiple prefix support you can add one or more
+  packname: 'GENE-XMD',
+  author: '‎Ransoms',
+  timeZone: 'Africa/Nairobi',
+  botName: "GENE-XMD", // your bot name
+  botOwner: 'Eugene', // set your name
+  ownerNumber: '254789696420', // Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "private",
+  maxStoreMessages: 20,
+  tempCleanupInterval: 1 * 60 * 60 * 1000, // 1 hours
+  storeWriteInterval: 10000,
+  description: "This is a bot is for personal use and misuse can lead to account suspension.",
+  version: "3.0.2",
+  updateZipUrl: "https://github.com/Eugene-Firm/GENE-XMD/archive/refs/heads/main.zip",
+  channelLink: "https://whatsapp.com/channel/0029VbBv9oEKmCPGHMigif1Z",
+  ytch: "Ransomske"
+};
+
+module.exports = settings;
+
